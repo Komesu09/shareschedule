9b0b8e5647c41740ac5823e038730f389012bd3a
Komesu09 <67633415+Komesu09@users.noreply.github.com>
2020-08-03 14:18:24 +0900

    change view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events
   root 'events#index'
   resources :users, only: [:index, :edit, :update]
   resources :my_calendars, only: [:new, :create,:index, :edit, :update]
+    resources :events
   resources :shares, only: [:new, :create,:index, :edit, :update]
+    resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
