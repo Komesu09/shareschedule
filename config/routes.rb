f3702f83ad8efaf2b2ba54f415aff5793a19741f
Komesu09 <67633415+Komesu09@users.noreply.github.com>
2020-08-03 16:38:02 +0900

    edit event-model

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'events#index'
   resources :users, only: [:index, :edit, :update]
-  resources :my_calendars, only: [:new, :create,:index, :edit, :update]
+  resources :my_calendars, only: [:new, :create,:index, :edit, :update] do
     resources :events
-  resources :shares, only: [:new, :create,:index, :edit, :update]
+  end
+  resources :events
+  resources :shares, only: [:new, :create,:index, :edit, :update] do
     resources :events
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
